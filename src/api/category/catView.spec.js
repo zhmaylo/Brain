30236c4c31ea02df58fd42acee06c7ced240bc61
zhmaylo <dzhmaylo@gmail.com>
2020-07-03 15:05:01 +0300

    20200703-1505

--- src/api/category/catView.spec.js
+++ src/api/category/catView.spec.js
@@ -15,52 +15,70 @@ test('"getMainListCategory" returns entry level categories (parentID=1). => ', (
     expect(data).toMatchObject(outList);
 });
 
-test('"getUpperListCategory" returns view upper list category (selectItem.name != SIGN_LEVEL_UP). => ', () => {
+
+test('"getListCategory" (getUpperListCategory) returns view upper list category (parentID = 1 ). => ', 
+() => {
 
     let entryList =
-        [{ categoryID: 1181, parentID: 1, isChildren: true, realcat: 0, name: "Ноутбуки, планшеты" },
-        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
-        { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
+        [{ categoryID: 0, parentID: 1181, isChildren: false, realcat: 0, name: SIGN_LEVEL_UP },
+        { categoryID: 1181, parentID: 1, isChildren: false, realcat: 0, name: "Ноутбуки" },
+        { categoryID: 1211, parentID: 1183, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
 
     let outList =
-        [{ categoryID: 0, parentID: 1181, isChildren: false, realcat: 0, name: SIGN_LEVEL_UP },
-        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
-        { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
-        
-        
-    let data = getUpperListCategory(entryList, entryList[0]);
-    console.log("getUpperListCategory", data);
-    expect(data).toMatchObject(outList);
-});
+        [{ categoryID: 1181, parentID: 1, isChildren: false, realcat: 0, name: "Ноутбуки" }];
 
-test('"getListCategory" returns view list category (isChildren = true). => ', () => {
-
-    let entryList =
-        [{ categoryID: 1181, parentID: 1, isChildren: true, realcat: 0, name: "Ноутбуки, планшеты" },
-        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
-        { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
-
-    let outList =
-        [{ categoryID: 0, parentID: 1181, isChildren: false, realcat: 0, name: SIGN_LEVEL_UP },
-        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
-        { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
-        
     let data = getListCategory(entryList, entryList[0]);
     console.log("getListCategory", data);
     expect(data).toMatchObject(outList);
 });
 
-test('"getListCategory" returns view list category  (isChildren = false && selectItem.name != SIGN_LEVEL_UP). => ', 
+test('"getListCategory" (getUpperListCategory) returns view upper list category (parentID != 1). => ', 
+() => {
+
+    let entryList =
+        [{ categoryID: 0, parentID: 1181, isChildren: false, realcat: 0, name: SIGN_LEVEL_UP },
+        { categoryID: 1181, parentID: 1182, isChildren: false, realcat: 0, name: "Ноутбуки" },
+        { categoryID: 1211, parentID: 1183, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
+
+    let outList =
+        [{ categoryID: 0, parentID: 1182, isChildren: false, realcat: 0, name: "<" },
+        { categoryID: 1181, parentID: 1182, isChildren: false, realcat: 0, name: "Ноутбуки" }];
+    // { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
+    // { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
+
+    let data = getListCategory(entryList, entryList[0]);
+    console.log("getListCategory", data);
+    expect(data).toMatchObject(outList);
+});
+
+
+test('"getListCategory (getUnderListCategory)" returns view list category (isChildren = true). => ', () => {
+
+    let entryList =
+        [{ categoryID: 1181, parentID: 1, isChildren: true, realcat: 0, name: "Ноутбуки, планшеты" },
+        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
+        { categoryID: 1211, parentID: 1183, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
+
+    let outList =
+        [{ categoryID: 0, parentID: 1181, isChildren: false, realcat: 0, name: SIGN_LEVEL_UP },
+        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" }];
+
+    let data = getListCategory(entryList, entryList[0]);
+    console.log("test getListCategory", data);
+    expect(data).toMatchObject(outList);
+});
+
+test('"getListCategory" returns view list category  (isChildren = false && selectItem.name != SIGN_LEVEL_UP). => ',
     () => {
-    let entryList =
-        [{ categoryID: 1181, parentID: 1, isChildren: false, realcat: 0, name: "Ноутбуки, планшеты" },
-        { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
-        { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
+        let entryList =
+            [{ categoryID: 1181, parentID: 1, isChildren: false, realcat: 0, name: "Ноутбуки, планшеты" },
+            { categoryID: 1191, parentID: 1181, isChildren: false, realcat: 0, name: "Ноутбуки" },
+            { categoryID: 1211, parentID: 1181, isChildren: false, realcat: 0, name: "Аксессуары для ноутбуков" }];
 
-    let data = getListCategory(entryList, entryList[0]);
-    console.log("getListCategory", data);
-    expect(data).toBe(false);
-});
+        let data = getListCategory(entryList, entryList[0]);
+        console.log("getListCategory", data);
+        expect(data).toBe(false);
+    });
 
 
 // tasca - testAsyncCallback Creates Async Callback test
--- src/api/category/symbChildren.spec.js
+++ src/api/category/symbChildren.spec.js
@@ -14,4 +14,17 @@ test('"setFieldIsChildren" set field "isChildren" to "true" or "false". => ', ()
     expect(catList[1].isChildren).toBe(false);
     expect(catList[2].isChildren).toBe(false);
 });
+test('"setFieldIsChildren" set field "isChildren" to "true" or "false". => ', () => {
+    let categoryList = [
+        { categoryID: 7271, parentID: 1, isChildren: true, realcat: 0, name: "Услуги" },
+        { categoryID: 8250, parentID: 1, isChildren: false, realcat: 0, name: "Новые классы" },
+        { categoryID: 7914, parentID: 7271, isChildren: false, realcat: 0, name: "Защита" }];
+
+    let catList = setFieldIsChildren(categoryList);
+    console.log("Test getCategoryList. catList", catList);
+    // console.log("Test getCategoryList. dipatch data", data);
+    expect(catList[0].isChildren).toBe(true);
+    expect(catList[1].isChildren).toBe(false);
+    expect(catList[2].isChildren).toBe(false);
+});
 
