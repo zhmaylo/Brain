1e247ff9c2b6915c76aac6aa7f2e3c68219ec277
Дмитрий Жмайло <dzhmaylo@gmail.com>
2020-08-30 17:37:20 +0300

    20200830-1740

--- src/constants/categoryConst.js
+++ src/constants/categoryConst.js
@@ -5,7 +5,7 @@
 export const NUM_COLUMN = 1;
 export const SIGN_DEEP = ">";
 export const SIGN_MAIN_LIST = 1;
-export const SIGN_LEVEL_UP = "< Назад";
+export const SIGN_LEVEL_UP = "< Выше";
 export const CATEG_HEADER_TITLE = 'Каталог';
 //
 // End. Section 'category list - setting list category'
