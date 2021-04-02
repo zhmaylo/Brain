15147bb1303e039fb174a2a29da19f4d9cd69d6b
zhmaylo <dzhmaylo@gmail.com>
2021-04-02 15:42:30 +0300

    20210402-1540

--- src/screen/filterScr.js
+++ src/screen/filterScr.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useContext, useState } from 'react';
-import { View, StyleSheet, StatusBar, SafeAreaView, Text, TextInput } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet, StatusBar, SafeAreaView, Text, TextInput, Button } from 'react-native';
 
 import { HeaderBack } from '../components/header/HeaderBackCmp';
 import { FooterBack } from '../components/footer/FooterBackCmp';
@@ -7,14 +7,10 @@ import { FOOTER_BACK_TITLE } from '../constants/footerBackConst';
 import { WINDOW_WIDTH } from '../constants/otherConst';
 import { ContextApp } from "../reducers/unionRdc";
 import { FILTER_HEADER_TITLE } from './../constants/filterConst';
-import RangeSlider, { Slider } from 'react-native-range-slider-expo';
+
 
 export default function filterScr(props) {
     const { state, dispatch } = useContext(ContextApp);
-
-    const [fromValue, setFromValue] = useState(0);
-    const [toValue, setToValue] = useState(0);
-    const [value, setValue] = useState(0);
 
     // getFilterProd()
     return (
@@ -23,28 +19,11 @@ export default function filterScr(props) {
             <StatusBar hidden={true} />
             <HeaderBack props={props} headerName={FILTER_HEADER_TITLE} />
             <View style={styles.item} >
-                <TextInput keyboardType='number-pad' defaultValue='input number' />
-                <View>
-                    <RangeSlider min={0} max={250000} step={100}
-                         fromValueOnChange={value => setFromValue(value)}
-                         toValueOnChange={value => setToValue(value)}
-                         initialFromValue={11}
-                         styleSize='large'
-                    />
-                    <Text>from value:  {fromValue}</Text>
-                    <Text>to value:  {toValue}</Text>
-               </View>
-               <View>
-                    <Slider min={0} max={40} step={4}
-                         valueOnChange={value => setValue(value)}
-                         initialValue={12}
-                         knobColor='red'
-                         valueLabelsBackgroundColor='black'
-                         inRangeBarColor='purple'
-                         outOfRangeBarColor='orange'
-                    />
-                    <Text>value:  {value}</Text>
-               </View>
+                <TextInput styles={{width:100, height:20}} keyboardType='number-pad' defaultValue='input number' />
+                <Button styles={{width:100, height:20}} title='Reset'/>
+                <Button styles={{width:100, height:20}} title='+100'/>
+                <Button styles={{width:100, height:20}} title='+1000'/>
+                <Button styles={{width:100, height:20}} title='+10000'/>
             </View>
 
             <FooterBack props={props} footerName={FOOTER_BACK_TITLE} />
