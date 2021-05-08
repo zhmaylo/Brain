efcb40aa472e9f687d50a7492d284dd92d919f3f
zhmaylo <dzhmaylo@gmail.com>
2021-05-08 18:31:11 +0300

    20210508-1830

--- src/api/search/search.spec.tsx
+++ src/api/search/search.spec.tsx
@@ -4,8 +4,9 @@ import { getSearchResult } from "./search";
 
 describe('getSearchResult - return search result', () => {
     test('getSearchResult - testing ... ', () => {
-        let products = PRODUCTS_FROM_FILE;
-        let outdata = getSearchResult(products, 'й + 7 (903)-123-45-67- ,. q wErt й цу кЕн A ბ ㄱ');
+        let products = PRODUCTS_FROM_FILE[0].result.list;
+
+        let outdata = getSearchResult(products, 'U0036411 79104');
         console.log(outdata);
         expect(outdata).toBe(0);
     });
--- src/api/search/search.tsx
+++ src/api/search/search.tsx
@@ -3,18 +3,72 @@
 // products - products list
 // request - search string
 export const getSearchResult = (products: any, request: string) => {
-    
-    return getSearchRegExp(request);
+    let combineItem: string;
+    let regExpArr = getRegExpArr(request);
+    let outProducts = [];
+    products.forEach((item: any) => {
+        combineItem = combine(item);
+        (isRequestInItem(regExpArr, combineItem)) && (outProducts.push(item));
+    });
+    return outProducts;
 };
 
-// getSearchRegExp - return search regular expression
-const getSearchRegExp = (request: string) => {
-        let searchRegExp : any;
-        // let regExp = new RegExp('\p{L}','i');
-        
-        searchRegExp = request.match(/[a-zа-я0-9]+/gui);
-        let regExp = new RegExp(searchRegExp,'/i');
-        let outReg = regExp.test('wErt')
-        return outReg;
+// isRequestInItem - checking if the request is in an element
+// requestArr - array of query substrings
+// combineItem - combine item fields
+const isRequestInItem = (regExpArr: any, combineItem: string) => {
+    let i = 0;
+    let testRes: any;
+    while (i < regExpArr.length) {
+        testRes = regExpArr[i].test(combineItem);
+        if (!testRes) {return false};
+        i++;
+    };
+    return true;
+};
+
+// getRegExpArr - return regular expression array
+// request - search string
+// return - array of regular expression 
+const getRegExpArr = (request: string) => {
+    // query split into substrings
+    let requestArr = request.match(/[a-zа-я0-9]+/gui);
+    // array of regular expression 
+    let regExpArr = new Array();
+    requestArr.forEach((item: any) => {
+        let regEx = new RegExp(item, 'iu');
+        regExpArr.push(regEx);
+    });
+    return regExpArr;
+};
+    
+
+// combine - item strings
+// item -product information
+// return - combine item fields
+const combine = (item: any) => {
+    return (
+        item.productID + ' '
+        + item.product_code + ' '
+        + item.articul + ' '
+        + item.name + ' '
+        + item.brief_description + ' '
+        + item.country
+    );
 }
 
+
+
+    // requestSplit - return search regular expression
+    // const arrayTo = (request: string) => {
+    //     return request.match(/[a-zа-я0-9]+/gui);
+
+        // let searchRegExp: any;
+        // // let regExp = new RegExp('\p{L}','i');
+
+        // let regExp = new RegExp(searchRegExp[3], 'i');
+        // // let outReg = regExp.test('wErt 123 12 13 12 e1233t')
+
+        // let outReg = 'wErt й 13 7 123 13 12 e1233t'.match('/'+searchRegExp[1]+'/'+searchRegExp[3]+'/');
+        // return outReg;
+    // };
